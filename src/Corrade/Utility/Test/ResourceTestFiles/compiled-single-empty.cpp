0deb03493fac1483e0e6590d9b3237339d484b3f
zhongtianzhou <zhongtianzhou@360.cn>
2023-03-09 14:11:09 +0800

    build lite

--- src/Corrade/Utility/Test/ResourceTestFiles/compiled-single-empty.cpp
+++
@@ -1,6 +0,0 @@
-/* Compiled resource file. DO NOT EDIT! */
-
-extern const unsigned int resourceSize_ResourceTestData = 0;
-extern const unsigned char resourceData_ResourceTestData[] = {
-    0x00
-};
